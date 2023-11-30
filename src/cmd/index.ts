3320c1d54ca4bfe8128a0878a5c4eae10f0d3f98
4lch4 <git@4lch4.email>
2023-11-30 17:03:51 -0600

    feat: added start of AliasesCommand

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -1,2 +1,12 @@
-export * from './config-files'
-export * from './create'
+import { Command } from 'commander'
+import { AliasesCommand } from './aliases'
+import { ConfigFilesCommand } from './config-files'
+import { buildCreateCommand } from './create'
+
+export async function buildCommands(): Promise<Command[]> {
+  const configFilesCommand = await new ConfigFilesCommand().build()
+  const aliasesCommand = await new AliasesCommand().build()
+  const createCommand = buildCreateCommand()
+
+  return [configFilesCommand, aliasesCommand, createCommand]
+}
